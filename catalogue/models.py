8ef37307c91c443f292dc85abef4c526234f0781
Mikhail Sobolev <mss@mawhrin.net>
2010-03-11 21:30:55 +0200

    first cut of the database model

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    ''' ... '''
+
+    title = models.TextField(null=False)
+
+    language = models.CharField(max_length=64, null=False)
+
+    # MSS: probably, this needs to be replaced with FileField or something
+    file = models.TextField(null=False)
+
+    class Meta:
+        ordering = [ 'title' ]
+
+class Author(models.Model):
+    ''' ... '''
+
+    name = models.CharField(max_length=256, null=False, unique=True)
+
+    class Meta:
+        ordering = [ 'name' ]
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book)
+    author = models.ForeignKey(Author)
+
+    class Meta:
+        unique_together = (
+            ('book', 'author'),
+        )
+
+class Series(models.Model):
+    name = models.CharField(max_length=256, null=False)
+
+    class Meta:
+        db_table = 'catalogue_series'
+        ordering = [ 'name' ]
+
+class BookSeries(models.Model):
+    book = models.ForeignKey(Book)
+    series = models.ForeignKey(Series)
+    number = models.IntegerField()
+
+    class Meta:
+        unique_together = (
+            ('book', 'series', 'number'),
+        )
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128, null=False, unique=True)
+    parent = models.ForeignKey('self', null=True)
+
+    class Meta:
+        ordering = [ 'name' ]
+
+class BookTag(models.Model):
+    book = models.ForeignKey(Book)
+    tag = models.ForeignKey(Tag)
+
+    class Meta:
+        unique_together = (
+            ('book', 'tag'),
+        )
+
+# vim:ts=4:sw=4:et
