09ffcc67b1df080885f6fa70a4ed686c8b0a7d96
Mikhail Sobolev <mss@mawhrin.net>
2010-03-14 23:05:07 +0200

    added a helper for opening zip files

--- formats/utils.py
+++ formats/utils.py
@@ -1,8 +1,25 @@
 
 import sys
-from traceback import print_exc
+import zipfile
 
 from xml.etree import ElementTree as ET
+
+from traceback import print_exc
+
+def get_good_zip(zipname):
+    ''' a helper function that returns an instance of ZipFile for a good zip file '''
+
+    try:
+        archive = zipfile.ZipFile(zipname)
+    except zipfile.BadZipfile:
+        print >> sys.stderr, '%s is not a zip file' % zipname
+        return
+
+    if archive.testzip():
+        print >> sys.stderr, '%s is broken' % zipname
+        return
+
+    return archive
 
 def parse_xml(data):
     try:
