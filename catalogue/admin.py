e093a44edd42aa0d50b86a5d5580a9294a72946a
Mikhail Sobolev <mss@mawhrin.net>
2010-03-14 21:38:41 +0200

    enabled admin interface for Author, Series, Tag

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from myLibrary.catalogue.models import Book
+from myLibrary.catalogue.models import Book, Author, Series, Tag
 
 admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Series)
+admin.site.register(Tag)
