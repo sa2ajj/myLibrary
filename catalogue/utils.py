fd38a05c00cf9b00426d56b8d23903b5951bd784
Mikhail Sobolev <mss@mawhrin.net>
2010-03-15 08:41:23 +0200

    print (for now) all available information about the found book

--- catalogue/utils.py
+++ catalogue/utils.py
@@ -1,7 +1,21 @@
 #
 
+def safe_text(text):
+    return text.encode('utf-8')
+
 def update_book(book_info):
     if book_info.valid:
-        print 'update_book: found a book:', book_info.path, book_info.stamp, book_info
+        print 'update_book: found a book:', book_info.path, book_info.stamp
+        print ' title:', safe_text(book_info.title)
+        print ' authors:'
+        for author in book_info.authors:
+            print '  ', safe_text(author)
+        print ' lang:', safe_text(book_info.language)
+        print ' series:'
+        for series in book_info.series:
+            print '  ', safe_text(series)
+        print ' tags:'
+        for tag in book_info.tags:
+            print '  ', safe_text(tag)
     else:
         print 'update_book: not a valid book at', book_info.path
