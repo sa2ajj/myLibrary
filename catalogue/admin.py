b8706b0622d5ba42c3cab757a16524911798e062
Mikhail Sobolev <mss@mawhrin.net>
2010-03-15 22:14:57 +0200

    added inline editing for BookAuthor in admin interface

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
 from myLibrary.catalogue.models import Book, Author, Series, Tag
+from myLibrary.catalogue.models import BookAuthor, BookSeries, BookTag
 
-admin.site.register(Book)
-admin.site.register(Author)
+class BookAuthorInline(admin.TabularInline):
+    model = BookAuthor
+    extra = 1
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = (BookAuthorInline,)
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = (BookAuthorInline,)
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Series)
 admin.site.register(Tag)
