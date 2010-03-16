80d76d9232edbb582452ced3c9a9384f3f819def
Mikhail Sobolev <mss@mawhrin.net>
2010-03-16 23:29:55 +0200

    added one more interesting book to check

--- test_single.py
+++ test_single.py
@@ -7,7 +7,11 @@ from catalogue.utils import print_info
 
 # book = FB2BookInfo('/Downloads/Books/other/Books/Vadim Panov/9- Кафедра странников/panov_tayiniyyi_gorod_9_kafedra_strannikov.fb2.zip')
 
-for filename in ('/Downloads/Books/epub/MakingThingsHappen.epub', '/Downloads/Books/Scalzi,_John_-_Old_Man\'s_War_04_-_Zoe\'s_Tale.epub'):
+for filename in (
+        '/Downloads/Books/epub/MakingThingsHappen.epub',
+        '/Downloads/Books/Scalzi,_John_-_Old_Man\'s_War_04_-_Zoe\'s_Tale.epub',
+        '/Downloads/Books/Carver - Strange Attractors.epub'
+    ):
     book = EpubBookInfo(filename)
 
     print 'Processing:', book.path
