7ed5e27d3ef81b3da6251a4435e5a59e0e201d71
Mikhail Sobolev <mss@mawhrin.net>
2010-03-16 23:11:56 +0200

    made a bit more complicated user of book formats

--- test_single.py
+++ test_single.py
@@ -1,18 +1,22 @@
 #! /usr/bin/python -tt
 # -*- coding: utf-8 -*-
 
-from formats.fb2 import FB2BookInfo
+#from formats.fb2 import FB2BookInfo
+from formats.epub import EpubBookInfo
 from catalogue.utils import print_info
 
-book = FB2BookInfo('/Downloads/Books/other/Books/Vadim Panov/9- Кафедра странников/panov_tayiniyyi_gorod_9_kafedra_strannikov.fb2.zip')
+# book = FB2BookInfo('/Downloads/Books/other/Books/Vadim Panov/9- Кафедра странников/panov_tayiniyyi_gorod_9_kafedra_strannikov.fb2.zip')
 
-print 'Processing:', book.path
-print '  last modified:', book.stamp
-print '  id:', book.id
+for filename in ('/Downloads/Books/epub/MakingThingsHappen.epub', '/Downloads/Books/Scalzi,_John_-_Old_Man\'s_War_04_-_Zoe\'s_Tale.epub'):
+    book = EpubBookInfo(filename)
 
-book.validate()
+    print 'Processing:', book.path
+    print '  last modified:', book.stamp
+    print '  id:', book.id
 
-if book.valid:
-    print_info(book)
-else:
-    print 'Invalid book'
+    book.validate()
+
+    if book.valid:
+        print_info(book)
+    else:
+        print 'Invalid book'
