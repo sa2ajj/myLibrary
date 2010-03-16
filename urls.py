fce8adca01ad615a8fa65f19a1f570e70b137eca
Mikhail Sobolev <mss@mawhrin.net>
2010-03-16 23:56:01 +0200

    enabled web view for people with browsers

--- urls.py
+++ urls.py
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     (r'^admin/', include(admin.site.urls)),
+
+    ('', include('myLibrary.web.urls')),
 )
