b92c1499d037cc59ea2893f1259d5d3277195c1f
Mikhail Sobolev <mss@mawhrin.net>
2010-03-17 21:50:40 +0200

    added a posibility to download files

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('myLibrary.web.views',
     (r'^$', 'index'),
-    (r'^book/(?P<id>.+)$', 'show_book')
+    (r'^book/(?P<id>.+)$', 'show_book'),
+    (r'^file/(?P<id>.+)$', 'download_file')
 )
--- web/views.py
+++ web/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, Http404
+import os
+
+from django.http import HttpResponse, Http404, HttpResponseNotAllowed, HttpResponseServerError
 from django.shortcuts import get_object_or_404, render_to_response
 
 from catalogue.models import Book, BookAuthor, BookTag
@@ -23,5 +25,29 @@ def show_book(request, id):
     tags = [ x.tag for x in BookTag.objects.filter(book=book).order_by('tag__name') ]
 
     return render_to_response('book.html', dict(book=book, authors=authors, tags=tags))
+
+def download_file(request, id):
+    id = _validate_id(id)
+
+    if request.method != 'GET':
+        return HttpResponseNotAllowed([ 'GET' ])
+
+    book = get_object_or_404(Book, id=id)
+
+    try:
+        content = open(book.file, 'rb')
+    except IOError:
+        return HttpResponseServerError('Cannot open the requested file')
+
+    data = content.read()
+    content.close()
+
+    response = HttpResponse(mimetype=book.mimetype)
+    response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(book.file)
+    response["Content-Length"] = len(data)
+
+    response.write(data)
+
+    return response
 
 # vim:ts=4:sw=4:et
