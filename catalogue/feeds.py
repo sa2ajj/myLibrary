fc9fbc26b4e55e77d2714596a0238f8fca80cf69
Mikhail Sobolev <mss@mawhrin.net>
2010-03-24 09:00:58 +0200

    first cut of feed related implementation

--- catalogue/feeds.py
+++ catalogue/feeds.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from xml.etree import ElementTree as ET
 
+from xmlutils import tostring
 from utils import LocalTimezone
 
 ATOM_NS = 'http://www.w3.org/2005/Atom'
@@ -12,47 +13,89 @@ DC_NS = 'http://purl.org/dc/elements/1.1/'
 
 FEED_MIME_TYPE = 'application/atom+xml'
 
-def add_child(parent, name, text_=None, **attrib):
-    """adds a child element with the specified parameters"""
-
-    child = ET.SubElement(parent, name, attrib=attrib)
-    if text_ is not None:
-        child.text = text_
-
-    return child
-
-def make_link(href, type_, rel, **other):
-    """creates a link element"""
-
-    attrib = {
-        'href': href,
-        'type': type_,
-        'rel': rel,
-    }
-    attrib.update(other)
-
-    return ET.Element('link', attrib=attrib)
-
 class FeedItem(object):
-    """Feed item"""
+    """base class for various feed related items"""
 
-    def __init__(self):
-        self._element = ET.Element('entry')
+    def __init__(self, name='entry', text_=None, attrib_=None, **other):
+        if attrib_ is None:
+            attrib = other
+        else:
+            attrib = attrib_
+            attrib.update(other)
 
-        self._element.append(self.uid)
-        self._element.append(self.title)
+        self._element = ET.Element(name, attrib=attrib)
+        if text_ is not None:
+            self._element.text = text_
+
+        self._good = False
+
+    def xml(self):
+        """returns xml representation"""
+
+        self._ensure_content()
+
+        return tostring(self._element)
 
     @property
     def element(self):
         """returns the ElementTree instance"""
 
+        self._ensure_content()
+
         return self._element
+
+    def _ensure_content(self):
+        """ensures content of the element"""
+
+        if not self._good:
+            self.add_child('id', self.uid)
+            self.add_child('title', self.title)
+
+            self.add_other_elems()
+
+            self._good = True
+
+    def add_element(self, element):
+        """adds the specified element as a child"""
+
+        assert ET.iselement(element)
+
+        self._element.append(element)
+
+    def add_child(self, name, text_=None, attrib_=None, **other):
+        """adds a child element with the specified parameters"""
+
+        if attrib_ is None:
+            attrib = other
+        else:
+            attrib = attrib_
+            attrib.update(other)
+
+        child = ET.SubElement(self._element, name, attrib=attrib)
+        if text_ is not None:
+            child.text = text_
+
+        return child
+
+    def add_link(self, href, type_, rel=None, **other):
+        """adds a link element"""
+
+        attrib = {
+            'href': href,
+            'type': type_,
+        }
+        if rel is not None:
+            attrib['rel'] = rel
+
+        attrib.update(other)
+
+        return self.add_child('link', attrib_=attrib)
 
     @property
     def uid(self):
-        """returns ET.Element with the item's id"""
+        """item's unique id"""
 
-        return ET.Element('id', self.get_uid())
+        return self.get_uid()
 
     def get_uid(self):
         """get the item's unique id"""
@@ -61,57 +104,69 @@ class FeedItem(object):
 
     @property
     def title(self):
-        """returns ET.Element with the item's title"""
+        """item's title"""
 
-        return ET.Element('id', self.get_title())
+        return self.get_title()
 
     def get_title(self):
         """get the item's title"""
 
         raise NotImplementedError, '%s has no implementation for get_title' % self.__class__.__name__
 
-class Feed(object):
+    def add_other_elems(self):
+        """add all other properties for the entry"""
+
+        raise NotImplementedError, '%s has no implementation for add_other_elems' % self.__class__.__name__
+
+class Feed(FeedItem):
     """represents an atom feed"""
 
     def __init__(self, link, title, updated=None):
-        self._feed = ET.Element('feed', attrib={
+        super(Feed, self).__init__('feed', attrib_={
             'xmlns': ATOM_NS,
             'xmlns:dc': DC_NS,
         })
 
-        add_child(self._feed, 'title', text_=title)
+        self._link = link
+        self._title = title
 
         if updated is None:
             updated = datetime.now(LocalTimezone())
 
-        add_child(self._feed, 'updated', text_=updated.isoformat())
-        self._feed.append(make_link(link, type_=FEED_MIME_TYPE, rel='self'))
+        self._updated = updated
 
-    @property
-    def element(self):
-        """ElementTree of the feed"""
+    def add_other_elems(self):
+        """adds items for the feed"""
 
-        return self._feed
+        self.add_child('updated', self._updated.isoformat())
+        self.add_link(self._link, type_=FEED_MIME_TYPE, rel='self')
+
+    def get_uid(self):
+        """returns feed's uid"""
+
+        return self._link
+
+    def get_title(self):
+        """returns feed's title"""
+
+        return self._title
 
     def add(self, item):
         """add an item to the feed"""
         assert isinstance(item, FeedItem)
 
-        self._feed.append(item.element)
+        self.add_element(item.element)
 
-    @property
-    def mimetype(self):
+    @staticmethod
+    def mimetype():
         """feed's mimetype"""
         return FEED_MIME_TYPE
-
-    def __unicode__(self):
-        return ET.tostring(self._feed, 'utf-8')
 
 class FeedItemFeed(FeedItem):
     """a feed item that is in turn another feed"""
 
     def __init__(self, name, link):
-        super(FeedItem, self).__init__()
+        super(FeedItemFeed, self).__init__()
 
         self._name = name
         self._link = link
@@ -124,18 +179,30 @@ class FeedItemFeed(FeedItem):
         """returns item's title"""
         return self._name
 
+    def add_other_elems(self):
+        """add other items for the feed entry"""
+
+        self.add_link(self.uid, type_=FEED_MIME_TYPE)
+
 class FeedItemBook(FeedItem):
     """a feed item for a book"""
 
     def __init__(self, book):
-        super(FeedItem, self).__init__()
+        super(FeedItemBook, self).__init__()
 
         self._book = book
 
     def get_uid(self):
         """returns book's unique id"""
 
+        return self._book.id
+
     def get_title(self):
         """returns book's title"""
 
         return self._book.title
+
+    def add_other_elems(self):
+        """add other items for the book entry"""
+
+# vim:ts=4:sw=4:et
