3757e80253a7d779840fd74a9769069b0c09919a
Mikhail Sobolev <mss@mawhrin.net>
2010-03-29 00:04:59 +0300

    pylinted

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,11 +1,24 @@
+#
+
+"""OPDS catalogue implementation"""
+
 from django.http import HttpResponse
 
 from catalogue.feeds import Feed, FeedItemFeed
 
+class GetURI:
+    """a helper to get relative URI"""
+
+    def __init__(self, request):
+        self._request = request
+
+    def __call__(self, uri):
+        return self._request.build_absolute_uri(uri)
+
 def index(request):
     """Root catalogue"""
 
-    get_uri = lambda x=None: request.build_absolute_uri(x)
+    get_uri = GetURI(request)
 
     feed = Feed(get_uri(), 'Index for the catalogue')
 
