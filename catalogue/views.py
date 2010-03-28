44b1eea5cacea6ffd07f0a90d90455b25671ac89
Mikhail Sobolev <mss@mawhrin.net>
2010-03-29 00:00:13 +0300

    somewhat useful root catalogue

--- catalogue/views.py
+++ catalogue/views.py
@@ -3,6 +3,18 @@ from django.http import HttpResponse
 from catalogue.feeds import Feed, FeedItemFeed
 
 def index(request):
-    feed = Feed(request.build_absolute_uri(), 'Index for the catalogue')
+    """Root catalogue"""
 
-    return HttpResponse(unicode(feed), mimetype=feed.mimetype)
+    get_uri = lambda x=None: request.build_absolute_uri(x)
+
+    feed = Feed(get_uri(), 'Index for the catalogue')
+
+    feed.add(FeedItemFeed('Books', get_uri('books')))
+    feed.add(FeedItemFeed('Authors', get_uri('authors')))
+    feed.add(FeedItemFeed('Tags', get_uri('tags')))
+    feed.add(FeedItemFeed('Series', get_uri('series')))
+    feed.add(FeedItemFeed('New Books', get_uri('new')))
+
+    return HttpResponse(feed.xml(), mimetype=feed.mimetype)
+
+# vim:ts=4:sw=4:et
