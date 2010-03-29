7dbdb0dbb96f35764c9f855a5eeae49058779d1d
Mikhail Sobolev <mss@mawhrin.net>
2010-03-29 21:17:41 +0300

    pylinted

--- formats/fb2/__init__.py
+++ formats/fb2/__init__.py
@@ -1,9 +1,13 @@
 
+"""BookInfo for a book in FB2 format"""
+
 from formats import BookInfo, BookInfoError
 
 from formats.fb2.reader import read
 
 class FB2BookInfo(BookInfo):
+    """BookInfo for a book in FB2 format"""
+
     def __init__(self, path):
         super(FB2BookInfo, self).__init__(path)
 
@@ -18,54 +22,74 @@ class FB2BookInfo(BookInfo):
 
         self._cover = None
 
-    @staticmethod
-    def format_name():
+    @classmethod
+    def format_name(cls):
+        """human readable format name"""
+
         return 'FB2'
 
-    @staticmethod
-    def supports(filename):
+    @classmethod
+    def supports(cls, filename):
+        """checks whether a file is supported"""
+
         return filename.endswith('.fb2.zip') or filename.endswith('.fb2')
 
     @property
     def valid(self):
+        """tells whether the instance is representing a valid book"""
+
         return self._valid
 
     def _fail_if_not_valid(self):
+        """raises an exception if the instances is not representing a valid book"""
+
         if not self._valid:
             raise BookInfoError('Attempt to get properties for a non-valid book')
 
     @property
     def language(self):
+        """book's language"""
+
         self._fail_if_not_valid()
 
         return self._language
 
     @property
     def title(self):
+        """book's title"""
+
         self._fail_if_not_valid()
 
         return self._title
 
     @property
     def authors(self):
+        """list of book's authors"""
+
         self._fail_if_not_valid()
 
         return self._authors
 
     @property
     def series(self):
+        """list of series the book belongs to"""
+
         self._fail_if_not_valid()
 
         return self._series
 
     @property
     def tags(self):
+        """list of book's tags"""
+
         self._fail_if_not_valid()
 
         return self._tags
 
     @property
     def mimetype(self):
+        """mime type for the book's file"""
+
         if not self.supports(self.path):
             raise BookInfoError('Mime-type requested for an unsupported file')
 
@@ -76,11 +100,15 @@ class FB2BookInfo(BookInfo):
 
     @property
     def annotation(self):
+        """book's annotation"""
+
         self._fail_if_not_valid()
 
         return self._annotation
 
     def validate(self):
+        """validates the instance"""
+
         if not self.supports(self.path):
             return
 
