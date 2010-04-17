0043ace45a91f98668458585449d7e5378649ffb
Mikhail Sobolev <mss@mawhrin.net>
2010-04-17 21:26:52 +0300

    added url definitions for the catalogue

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,4 +2,14 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('myLibrary.catalogue.views',
     (r'^$', 'index'),
+    (r'^book$', 'list_books'),
+    (r'^book/(?P<id>.+)$', 'show_book'),
+    (r'^author$', 'list_authors'),
+    (r'^author/(?P<id>.+)$', 'show_author'),
+    (r'^tag$', 'list_tags'),
+    (r'^tag/(?P<id>.+)$', 'show_tag'),
+    (r'^series$', 'list_series'),
+    (r'^series/(?P<id>.+)$', 'show_series')
 )
+
+# vim:ts=4:sw=4:et
