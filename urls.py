a7430a8e919612640134bc4c90ead93faa64fa85
Mikhail Sobolev <mss@mawhrin.net>
2010-11-29 22:13:11 +0200

    finalized main urls for now

--- urls.py
+++ urls.py
@@ -6,18 +6,19 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^catalogue/', include('myLibrary.catalogue.urls')),
+    ('', include('myLibrary.web.urls')),
+
+    # (r'^openid/$', 'django_openidconsumer.views.begin', { 'sreg': 'nickname' }),
+    # (r'^openid/complete/$', 'django_openidconsumer.views.complete'),
+    # (r'^openid/signout/$', 'django_openidconsumer.views.signout'),
+
+    ('^accounts/login/?$', 'django.contrib.auth.views.login'),
+    ('^accounts/logout/?$', 'django.contrib.auth.views.logout'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     (r'^admin/', include(admin.site.urls)),
-
-    (r'^openid/$', 'django_openidconsumer.views.begin', { 'sreg': 'nickname' }),
-    (r'^openid/complete/$', 'django_openidconsumer.views.complete'),
-    (r'^openid/signout/$', 'django_openidconsumer.views.signout'),
-
-    ('', include('myLibrary.web.urls')),
 )
 
 if settings.SERVE_MEDIA:
